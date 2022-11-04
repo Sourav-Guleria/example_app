ed0417eda08ac0e4ec04f198a656603d3dbcbc45
Sourav <souravguleria000@gmail.com>
2022-11-04 23:26:56 +0530

    second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp100());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp100 extends StatelessWidget {
+  const MyApp100({super.key});
 
   // This widget is the root of your application.
   @override
