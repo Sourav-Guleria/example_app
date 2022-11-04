4f4c3914ecb6c70fb9b73baa2fe50b388b1e0be0
Sourav-Guleria <83588553+Sourav-Guleria@users.noreply.github.com>
2022-11-04 23:37:04 +0530

    changing back

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp100());
+  runApp(const MyApp());
 }
 
-class MyApp100 extends StatelessWidget {
-  const MyApp100({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
